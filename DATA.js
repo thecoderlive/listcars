19ee1e3a5b68fdd6b0694a16807aa3a2d0c110d2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-02 11:35:24 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,195 @@
+export const item = {
+    "id": "f1b024ae-b8c7-4bc3-b4e7-f2d5a8f455b4",
+    "peace_list": [
+        {
+            "id": "dccd5c87-07ad-4383-a120-bd4d7f854617",
+            "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png",
+            "ready_title": "Social memory phone issue. Individual low deal. Life level gas these brother know detail everyone.",
+            "about_time": "Certain win ask American cultural. Behind weight room rise record Congress meeting.",
+            "detail_list": [
+                {
+                    "id": "84e1cd2e-58dd-4049-9a8d-6827e68f1161",
+                    "serial_number": 2372,
+                    "license_title": "Our model magazine type. Night pressure some by capital article big.",
+                    "about_license": "Like whose cover relationship surface let. Stock he attorney discuss."
+                },
+                {
+                    "id": "23e22c6e-bd65-4fd1-91c1-0c69ed18c0ce",
+                    "serial_number": 9049,
+                    "license_title": "Interesting build dinner minute after toward remember. Return use help.",
+                    "about_license": "Stand test operation hospital might week attack population. Realize question amount moment laugh."
+                },
+                {
+                    "id": "de4b52ec-8c95-4245-9a4a-6bb13f59f10c",
+                    "serial_number": 5129,
+                    "license_title": "Husband discover account what. Many her child.",
+                    "about_license": "Politics understand herself none himself."
+                },
+                {
+                    "id": "4c9ff3df-3805-4a2a-96db-f5cc62548704",
+                    "serial_number": 7013,
+                    "license_title": "Course American quickly allow body what. Top probably stuff. Edge under your shake you.",
+                    "about_license": "Bar beautiful enter hope have. Significant nearly his."
+                },
+                {
+                    "id": "d193f2aa-db5e-4645-9cca-764399f4de53",
+                    "serial_number": 8425,
+                    "license_title": "Also song letter once pull campaign. Onto military start during message local teach third. Type mention method cover ask. Officer dog life hotel.",
+                    "about_license": "In cup foot idea western mother."
+                }
+            ]
+        },
+        {
+            "id": "a8d71f3e-3f16-46d6-b6f5-dc7591e2fa8d",
+            "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/292.png",
+            "ready_title": "May network outside kitchen.",
+            "about_time": "Inside international all hear special training friend. Son join smile decade.",
+            "detail_list": [
+                {
+                    "id": "e187ef8c-ab48-4fc4-b635-4228f3a95fcd",
+                    "serial_number": 8762,
+                    "license_title": "His treatment field rule current respond. Part rule break know sense join stock.",
+                    "about_license": "Say main point join. Follow choice show."
+                },
+                {
+                    "id": "0ca23130-b8ec-4e41-b4c7-4b60111ccbdf",
+                    "serial_number": 3817,
+                    "license_title": "Law important claim as. Science hand class particularly relationship.",
+                    "about_license": "Future born respond become particularly another. Fly inside agency really civil quite."
+                },
+                {
+                    "id": "f04a12a1-8b13-481f-bd5d-0b5c1e3dca53",
+                    "serial_number": 3079,
+                    "license_title": "Resource point million. Issue care against cultural beautiful of.",
+                    "about_license": "Want minute couple win sort. Model popular reach speak need seem local total."
+                },
+                {
+                    "id": "b3787529-c0fe-4997-a5ab-4cbd4b28f2d5",
+                    "serial_number": 669,
+                    "license_title": "Woman son treatment. Partner thing wide evidence space. Walk scientist area recently then director.",
+                    "about_license": "Character ability season personal military white. Sit situation under experience. Sound its condition major capital stay nature."
+                },
+                {
+                    "id": "f51c30dc-95b3-4e7a-894f-8b5b56717bca",
+                    "serial_number": 2099,
+                    "license_title": "Television century on visit decision pressure often truth. Open necessary open exist thank face law. Mind reduce lead quality professor particularly. Many trouble themselves skill start majority.",
+                    "about_license": "Enough summer society popular indicate. Produce read security carry."
+                }
+            ]
+        },
+        {
+            "id": "d0a00b07-f11a-48a9-9153-73a709ee82e6",
+            "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png",
+            "ready_title": "Safe reach nothing arm poor prove. Address sister base simple maybe.",
+            "about_time": "Side never high. Degree test manager information ten everybody. Final effect away success stay.",
+            "detail_list": [
+                {
+                    "id": "4f8f4a3c-d9e8-47b3-afc8-db99295831f0",
+                    "serial_number": 3688,
+                    "license_title": "Walk staff chair plan require.",
+                    "about_license": "Box all job anything attorney scientist mouth and. Herself different brother land light expect."
+                },
+                {
+                    "id": "5c84b5fd-101c-474e-a0b8-63860e1dab62",
+                    "serial_number": 4661,
+                    "license_title": "Political from study officer certainly. Threat coach around station.",
+                    "about_license": "Add heavy find road oil look image. Southern worry evening participant current charge. Section know read support food consider team notice."
+                },
+                {
+                    "id": "3e1d4c6c-7d44-40d2-b5c9-71495a38712a",
+                    "serial_number": 6206,
+                    "license_title": "Administration billion wonder enough your after chance think. Rise service fire day partner forward.",
+                    "about_license": "Society million of minute three. Arrive environmental available both same."
+                },
+                {
+                    "id": "ea6b7e00-072b-40f8-a8bd-a968dfcc5b55",
+                    "serial_number": 4472,
+                    "license_title": "Admit may get remember learn activity wife. Sense wife most game student third manage after.",
+                    "about_license": "Want size office city thing. People reflect land politics. Necessary whom daughter here."
+                },
+                {
+                    "id": "7114632d-9c41-4d9f-856d-2c6e3e648d58",
+                    "serial_number": 6974,
+                    "license_title": "Down up crime perhaps remain interest national tonight. Final record common hand. Deal call knowledge now president lawyer.",
+                    "about_license": "Happen have book message magazine. Discover would east word resource any note. Threat notice street admit."
+                }
+            ]
+        },
+        {
+            "id": "9ef35d1d-3ec0-47df-a421-c8b303bdc3bf",
+            "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png",
+            "ready_title": "Land lawyer tax although religious. Win nor another.",
+            "about_time": "You which thing resource. Member set defense alone. Crime experience next score specific without charge.",
+            "detail_list": [
+                {
+                    "id": "aa832b34-a013-4517-8b49-a20310795a01",
+                    "serial_number": 1322,
+                    "license_title": "They option analysis meet indicate off common. Clearly stay dog mother manage. Such protect accept avoid blood thought night what.",
+                    "about_license": "Society down field art. Poor language space capital. Meet decide benefit generation walk sing."
+                },
+                {
+                    "id": "04fa0b78-3c38-4fc4-9e0d-741409682e1d",
+                    "serial_number": 5912,
+                    "license_title": "Age sell cover major happen size. His five million piece story pass card. Theory want job exactly term future leg check.",
+                    "about_license": "Close so information including I. Feeling maintain option hear at talk. Since him rate money."
+                },
+                {
+                    "id": "3b395641-6b19-4fe8-b0da-eedefe28f98f",
+                    "serial_number": 1496,
+                    "license_title": "Still thank such when least evening. By read listen level brother Mr hundred something.",
+                    "about_license": "Ground beat above general. Key organization along someone."
+                },
+                {
+                    "id": "63b7b515-b502-4b76-9dc8-321e1a321be1",
+                    "serial_number": 4515,
+                    "license_title": "Compare rate term news office production. Very hair theory book on.",
+                    "about_license": "Within particularly community teach teacher. Still third idea party hit brother take."
+                },
+                {
+                    "id": "07ea30cf-0dd0-4ff6-b8da-f8455e2b351d",
+                    "serial_number": 8603,
+                    "license_title": "Her trial yes military. Itself own step assume national. Dream ask executive maintain forward sing produce last. Model production develop left number.",
+                    "about_license": "Adult able ago write cell. Current all reveal quality. Production maintain see."
+                }
+            ]
+        },
+        {
+            "id": "bd62de2c-6212-40d7-843a-a9af70ba01a0",
+            "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png",
+            "ready_title": "Her trial yes military. Itself own step assume national. Dream ask executive maintain forward sing produce last. Model production develop left number.",
+            "about_time": "Until size recognize out reach environment. Forget a own. Responsibility no a hair process.",
+            "detail_list": [
+                {
+                    "id": "9e1cd0fb-b7c0-450e-9add-febd51797033",
+                    "serial_number": 877,
+                    "license_title": "Reflect Democrat until toward international. Image relate course call organization general politics. Name mean pay wait suffer anyone government.",
+                    "about_license": "Simple board both. Medical education president worker easy discuss office. Send lot brother network staff."
+                },
+                {
+                    "id": "dbfbd71e-249a-491b-8aec-27293938f5c6",
+                    "serial_number": 8056,
+                    "license_title": "Full learn recent institution us commercial. Month care sort fear anyone green camera. Theory laugh under before.",
+                    "about_license": "Know size sit amount environmental wrong. Dinner TV color soon. Skill up message woman."
+                },
+                {
+                    "id": "8cc357c1-8a0a-452d-8dc1-cfd7f961ccd3",
+                    "serial_number": 7412,
+                    "license_title": "Behavior book too economic care perhaps mean. Pick she argue never soldier key. Floor evidence light TV glass treatment some.",
+                    "about_license": "Short just statement together. Different would still more. Above performance decade."
+                },
+                {
+                    "id": "2ebf0128-79f0-4b53-a12b-23e20681e939",
+                    "serial_number": 388,
+                    "license_title": "Inside international all hear special training friend. Son join smile decade.",
+                    "about_license": "Assume whose my let Mr respond environment politics. Generation purpose on professional time six."
+                },
+                {
+                    "id": "fa3477f6-957d-4267-a7ae-3d7bc5398b29",
+                    "serial_number": 4890,
+                    "license_title": "Community medical bad step Mrs listen. Help bed win fall high executive.",
+                    "about_license": "Politics understand herself none himself."
+                }
+            ]
+        }
+    ]
+}
